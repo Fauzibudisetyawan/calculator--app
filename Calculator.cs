79db44a40bb1fb0554719dbe19aac714890bddd3
fauzi budi setyawan <fauzibudisetyawan@students.amikom.ac.id>
2024-03-05 12:11:12 +0700

    menambahkan kode

---
+++ Calculator.cs
@@ -0,0 +1,18 @@
+class calculator
+{  
+    static void main(string[] args)
+	  {
+        int a = 10;
+		int b = 6;
+ 
+	  }  
+	  
+}
+static int penambahan(int a,int b) 
+{
+	return a + b;
+}
+static int pengurangan(int a,dan b)
+{
+	retrun a- b;
+}	
